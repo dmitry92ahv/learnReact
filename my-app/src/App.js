1f9baff6ac3790049d2a4ba4e78a681c26a4834e
Ahvatkin Dmitry <dmitry92ahv@gmail.com>
2022-01-07 18:03:13 +0600

    create App, run hello world

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+            Hello world!
+        </div>
+        <button>Click</button>
+    </div>
+  );
+}
+
+export default App;
