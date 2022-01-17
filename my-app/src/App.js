2a7868aa04dfd0887c99aed57ec8fa6e82f7da14
Ahvatkin Dmitry <dmitry92ahv@gmail.com>
2022-01-17 22:58:17 +0600

    first react app, learn simple component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
+
+
+  const [value, setValue] = useState('some text');
+
+
   return (
     <div className="App">
-        <div>
-            Hello world!
-        </div>
-        <button>Click</button>
+
+        <h1>{value}</h1>
+        <input value={value} 
+          onChange={event => setValue(event.target.value)}
+        ></input>
+
+        <Counter/>
+
+        <Counter/>
+
+        <Counter/>
+
     </div>
   );
 }
