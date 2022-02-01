7036da79910e1afdbd94cb24d7927c89dd28b473
DmitryAhv <dmitry92ahv@gmail.com>
2022-02-01 23:39:07 +0600

    create component PostList and transfer array like props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,32 @@ import React, {useState} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript', body: 'Description'},
+    {id: 3, title: 'Javascript', body: 'Description'},
+    {id: 4, title: 'Javascript', body: 'Description'},
+    {id: 5, title: 'Javascript', body: 'Description'},
+    {id: 6, title: 'Javascript', body: 'Description'},
+    {id: 7, title: 'Javascript', body: 'Description'},
+  ])
 
-  const [value, setValue] = useState('some text');
-
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'C#', body: 'Description'},
+    {id: 2, title: 'Python', body: 'Description'},
+  ])
+  
 
   return (
     <div className="App">
-
-        <h4>{value}</h4>
-        <input value={value} 
-          onChange={event => setValue(event.target.value)}
-        ></input>
-
-        <Counter/>
-        <ClassCounter/>
-
- 
-        <PostItem post = {{id: 1, title: 'Javascript', body: 'Description'}}/>
-        <PostItem post = {{id: 2, title: 'Test', body: 'Description tests'}}/>
-        <PostItem post = {{id: 3, title: 'Hi', body: 'How are u?'}}/>
+        <PostList posts={posts} title ="Список постов 1"/>
+        <PostList posts={posts2} title ="Список постов 2"/>
     </div>
-
-
   );
 }
 
